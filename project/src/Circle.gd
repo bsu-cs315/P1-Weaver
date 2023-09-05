9b7bfc40a7378711bb0d97f93d0a13b37f77ccbc
Kaleb Weaver <kcweaver2@bsu.edu>
2023-09-05 08:05:38 -0400

    Change launch system to click and drag controls

--- project/src/Circle.gd
+++ project/src/Circle.gd
@@ -1,12 +1,15 @@
 extends RigidBody2D
 
-@export var strength : float = 500
 
-func _process(delta):
-	if Input.is_action_just_pressed("launch"):
-		print('yes')
-		var direction = Vector2(1, -1).normalized()
-		var velocity = direction * strength
-		print (velocity)
-		apply_central_impulse(velocity)
-	
+var dragging
+var drag_start = Vector2()
+
+func _input(event):
+	if event.is_action_pressed("click") and not dragging:
+		dragging = true
+		drag_start = get_global_mouse_position()
+	if event.is_action_released("click") and dragging:
+		dragging = false
+		var drag_end = get_global_mouse_position()
+		var launch_dir = drag_start - drag_end
+		apply_central_impulse(launch_dir * 5)
