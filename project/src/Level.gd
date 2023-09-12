39db303ab6211f88ef452e1c30c077e8128c5ef6
Kaleb Weaver <kcweaver2@bsu.edu>
2023-09-12 09:50:09 -0400

    Create   m of 3 balls that have a 1 shot limit and allow you to only fire one at a time

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -1,1 +1,27 @@
 extends Node2D
+
+var circle = preload("res://src/Circle.tscn")
+var circleCounter = 3
+
+func _ready():
+	inst(Vector2(400,600))
+	$Circle.connect("circle_stop", on_state_changed)
+	
+func inst(pos):
+	if circleCounter > 0:
+		var instance = circle.instantiate()
+		instance.position = pos
+		add_child(instance)
+		circleCounter -=1
+		
+
+func on_state_changed():
+	
+	$Timer.start(7)
+
+	
+
+func _on_timer_timeout():
+	
+	remove_child($Circle)
+	inst(Vector2(400,600))
